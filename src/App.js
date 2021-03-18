b86b60474dc8d9f82076cb24c1cebc93d0db94c3
shikha <roy.shikha579@gmail.com>
2021-03-18 18:27:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from "react-dom";
 import './App.css';
+import Example from './Example';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+      <div className="row fixed-top pt-2 pb-2 bg-dark text-white shadow">
+        <div className="col-md-12">
+          <h4 className="text-center">
+            A simple virtual keyboard built with react.
+          </h4>
+        </div>
+      </div>
+      <div className="container-fluid demo-app">
+        <div className="row mt-5">
+          <div className="col-md-12">
+            <Example />
+          </div>
+        </div>
+        </div>
+    </React.Fragment>
     </div>
   );
 }
